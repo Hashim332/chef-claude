f476b4662a9f16f619c203e7ef489a990de25295
Hashim Rafiq <hashim.rafiq332@gmail.com>
2025-02-27 00:25:50 +0000

    updated header, loading function and form submission

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,15 +4,12 @@ import ClaudeRecipe from "./ClaudeRecipe";
 import { getRecipeFromChefClaude } from "../ai";
 
 export default function Main() {
-  const [ingredients, setIngredients] = React.useState([
-    "chicken",
-    "all the main spices",
-    "corn",
-    "heavy cream",
-    "pasta",
-  ]);
+  const [ingredients, setIngredients] = React.useState([]);
+
   const [recipe, setRecipe] = React.useState("");
   const recipeSection = React.useRef(null);
+
+  const [isLoading, setIsLoading] = React.useState(false);
 
   React.useEffect(() => {
     if (recipe !== "" && recipeSection.current !== null) {
@@ -21,8 +18,16 @@ export default function Main() {
   }, [recipe]);
 
   async function getRecipe() {
-    const recipeMarkdown = await getRecipeFromChefClaude(ingredients);
-    setRecipe(recipeMarkdown);
+    console.log("Setting loading to true");
+    setIsLoading(true);
+    try {
+      const recipeMarkdown = await getRecipeFromChefClaude(ingredients);
+      setRecipe(recipeMarkdown);
+    } catch (error) {
+      console.error("Error fetching recipe:", error);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   function addIngredient(formData) {
@@ -32,18 +37,24 @@ export default function Main() {
 
   return (
     <main>
+      <h2>Enter at least 4 ingredients to get a recipe!</h2>
       <form action={addIngredient} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
           name="ingredient"
+          required
         />
         <button>Add ingredient</button>
       </form>
 
       {ingredients.length > 0 && (
-        <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
+        <IngredientsList
+          ingredients={ingredients}
+          getRecipe={getRecipe}
+          isLoading={isLoading}
+        />
       )}
 
       {recipe && (
